116297152f8d41e3ca95820cab7f52a5418c9e91
mrevilpig <wadeliao1986@Macintosh.local>
2014-04-07 13:39:25 +0800

    Set up M:M association. Changes including category.rb, post.rb, post_category.rb and migrate post_categories

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,2 +1,4 @@
 class Category < ActiveRecord::Base
-end
+	has_many :post_categories
+	has_many :posts, through: :post_categories
+end 
---
+++ db/migrate/20140407051402_create_post_categories.rb
@@ -0,0 +1,12 @@
+class CreatePostCategories < ActiveRecord::Migration
+  def up
+    create_table :post_categories do |t|
+    	t.integer :post_id
+    	t.integer :category_id
+    end
+  end
+
+  def down
+  	drop_table :post_categories
+  end
+end
