3b065f84107d1fd81a63b1bbc7264a239c2d7cd4
mrevilpig <wadeliao1986@Macintosh.local>
2014-04-10 18:54:23 +0800

    ch2.9: Set up the category controller, views and models

---
+++ app/controllers/categories_controller.rb
@@ -0,0 +1,20 @@
+class CategoriesController < ApplicationController
+	def show
+		@category = Category.find(params[:id])
+	end
+
+	def new
+		@category = Category.new
+	end
+
+	def create
+		@category = Category.new(params.require(:category).permit(:name))
+
+		if @category.save
+			flash[:notice] = "Category has been created"
+			redirect_to posts_path
+		else
+			render :new
+		end
+	end
+end
--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,10 +1,11 @@
 class PostsController < ApplicationController
+	before_action :set_post, only: [:show, :edit, :update]
+
   def index
   	@posts = Post.all
   end
 
   def show
-  	@post = Post.find(params[:id])
   end
 
   def new
@@ -13,6 +14,7 @@ class PostsController < ApplicationController
 
   def create
   	@post = Post.new(post_params)
+  	@post.user = User.first
 
   	if @post.save
   		flash[:notice] = "Post created!!"
@@ -24,15 +26,12 @@ class PostsController < ApplicationController
   end
 
   def edit
-  	@post = Post.find(params[:id])
   end
 
   def update
-  	@post = Post.find(params[:id])
-
   	if @post.update(post_params)
   		flash[:notice] = "Update Success"
-  		redirect_to posts_path
+  		redirect_to post_path(@post)
   	else
   		render :edit
   	end
@@ -42,5 +41,9 @@ class PostsController < ApplicationController
   
   def post_params
   	params.require(:post).permit!
+  end
+
+  def set_post
+  	@post = Post.find(params[:id])
   end
 end
