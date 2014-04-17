cc9b8187046d647920433bacfcf343de89adabdc
mrevilpig <wadeliao1986@Macintosh.local>
2014-04-17 15:29:06 +0800

    Add bcrypt-ruby in Gemfile. Add has_user_password line and 2 validates in User model. Add a column password_digest

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,7 @@ class User < ActiveRecord::Base
 	has_many :posts
 	has_many :comments
 
+	has_secure_password validations: false
+	validates :username, presence: true, uniqueness: true
+	validates :password_digest, presence: true
 end
