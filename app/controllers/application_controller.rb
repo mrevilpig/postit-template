80fe46236d59b36bb964f4e847752ca41be3dfc5
mrevilpig <wadeliao1986@Macintosh.local>
2014-04-24 15:26:45 +0800

    Fix issue. Now each user can only edit their own post

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,7 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
-  helper_method :current_user, :logged_in?
+  helper_method :current_user, :logged_in?, :is_user?
 
   def current_user
   	@current_user ||= User.find(session[:user_id]) if session[:user_id]
@@ -11,6 +11,11 @@ class ApplicationController < ActionController::Base
 
   def logged_in?
   	!!current_user
+  end
+
+  def is_user?
+    post = Post.find(params[:id])
+    !!(current_user == post.user)
   end
 
   def require_user
